3b30fee31d02c6d9d1c23d5d15f3263b04bc9353
priti620 <121668165+priti620@users.noreply.github.com>
2023-08-09 16:17:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "google_sql_database_instance" "instance" {
-name = "my-database-instance"
+name = "database-instance"
 database_version = "MYSQL_8_0"
 region = "${var.region}"
 settings {
@@ -7,13 +7,13 @@ tier = "db-f1-micro"
 }
 }
 resource "google_sql_database" "database" {
-name = "mydatabase"
+name = "mydatabase1"
 instance = "${google_sql_database_instance.instance.name}"
 charset = "utf8"
 collation = "utf8_general_ci"
 }
 resource "google_sql_user" "users" {
-name = "root"
+name = "root1"
 instance = "${google_sql_database_instance.instance.name}"
 host = "%"
 password = "mypassw0rd"
